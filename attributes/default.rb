b7b696399bece63b2d363d4912d3f079b2a17045
Artem Ivantsov <aivantsov@qubell.com>
2016-01-10 15:02:09 +0200

    fix maven download url;move maven version to attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@ default['scm']['provider'] = "git"
 default['cookbook-qubell-build']['dest_path'] = "/tmp"
 default['cookbook-qubell-build']['dest_name'] = "ROOT"
 default['cookbook-qubell-build']['target'] = "/tmp/mvn"
+default['cookbook-qubell-build']['maven_version'] = "3.2.1"
 default['cookbook-qubell-build']['artifacts'] = []
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name "cookbook-qubell-build"
 version "0.3.0"
 
-depends "git", "2.7.1"
+depends "git", "2.7.0"
 depends "java", "1.11.7"
 depends "yum", "2.3.1"
-depends "apt", "2.3.9"
+depends "apt", "2.7.0"
